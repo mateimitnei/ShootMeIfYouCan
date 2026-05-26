fe8ce5742561af415c307fbadad6e1e460cc719f
Matei Lil Laptop <matei.mitnei@stud.acs.upb.ro>
2026-05-26 16:17:07 +0300

    Added IDLE_STATE, uart debugging and refined logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include <TM1637Display.h>
 
 // Pins
-const uint8_t PIN_BUZZER = 2;
-const uint8_t PIN_BUTTON = 3;
+#define PIN_BUZZER 2
+#define PIN_BUTTON 3
+#define PIN_TM_CLK 8
+#define PIN_TM_DIO 9
 const uint8_t PIN_SERVO[3] = {5, 6, 7};
-const uint8_t PIN_LDR[3] = {A0, A1, A2};
+const uint8_t PIN_LDR[3] = {14, 15, 16};
 const uint8_t PIN_LED[3] = {10, 11, 12};
-const uint8_t PIN_TM_CLK = 8;
-const uint8_t PIN_TM_DIO = 9;
 
 // Objects from included hardware libs
 Servo motors[3];
@@ -17,6 +17,7 @@ TM1637Display display(PIN_TM_CLK, PIN_TM_DIO);
 
 // Game logic states
 enum GameState {
+    IDLE_STATE,
     INIT_STATE,
     STANDBY_STATE,
     COUNTDOWN_STATE,
@@ -24,7 +25,7 @@ enum GameState {
     WIN_STATE,
     LOSS_STATE
 };
-GameState game_state = INIT_STATE;
+GameState game_state = IDLE_STATE;
 
 // Individual target states
 enum TargetState {
@@ -54,7 +55,6 @@ uint32_t time_standby_animation = 0;
 uint32_t time_start_state = 0;
 uint32_t time_last_frame = 0;
 
-bool calibrated = 0;
 uint16_t calibration_samples = 0;
 uint8_t current_frame = 0;
 uint8_t countdown_value = 3;
@@ -63,8 +63,10 @@ uint8_t countdown_value = 3;
 uint8_t buzzer_step = 0;
 uint8_t score_pos = 99;
 
+bool calibrated = 0;
 bool standby_animation_frame = false;
-// Initial button states: not pressed
+
+// Initial button state: not pressed
 bool btn_state = HIGH;
 bool prev_btn_reading = HIGH;
 uint32_t last_debounce_time = 0;
@@ -163,6 +165,8 @@ void game_init() {
         digitalWrite(PIN_LED[i], LOW);
     }
     game_state = INIT_STATE;
+    misses = 0;
+    score = 0;
     calibrated = 0;
     calibration_samples = 0;
     time_start_state = millis();
@@ -195,14 +199,14 @@ void check_button_press(uint32_t now) {
 
             // Pressed button:
             if (btn_state == LOW) {
-                // Pressed to start the game:
+                // Pressed to start playing:
                 if (game_state == STANDBY_STATE) {
                     lower_all_targets();
                     time_start_state = 0;
                     game_state = COUNTDOWN_STATE;
                 }
-                // Pressed to restart the game:
-                else if (game_state != INIT_STATE) {
+                // Pressed to (re)start the game and calibrate sensors:
+                else {
                     tone(PIN_BUZZER, 500, 300);
                     game_init();
                     time_start_state = now;
@@ -294,7 +298,6 @@ void countdown_state(uint32_t now) {
 
     // Move to PLAYING_STATE after 'GO'
     if (countdown_value == 0 && now - time_start_state >= 1000) {
-        score = 0;
         display_score(score);
         for (int i = 0; i < 3; i++) {
             targets[i].state = WAITING_TARGET;
@@ -325,8 +328,8 @@ void playing_state(uint32_t now) {
         else if (targets[i].state == READY_TARGET) {
             // Read light level
             uint32_t current_light = analogRead(PIN_LDR[i]);
-            // Send measured light to serial every 250 ms for debugging
-            if (now - last_print_time[i] > 250) {
+            // Send measured light to serial every 500 ms for debugging
+            if (now - last_print_time[i] > 500) {
                 Serial.print("Target "); Serial.print(i);
                 Serial.print(" light: "); Serial.println(current_light);
                 last_print_time[i] = now;
@@ -350,8 +353,12 @@ void playing_state(uint32_t now) {
 
             // Check if target has been up for too long
             if (now - targets[i].time_start_rising > 3000) {
-                lower_target(i, now);
                 misses++;
+                lower_target(i, now);
+                tone(PIN_BUZZER, 200, 100);
+                Serial.print("MISSED TARGET "); Serial.print(i);
+                Serial.print(" -> Misses: "); Serial.println(misses);
+                
                 if (misses >= 3) {
                     game_state = LOSS_STATE;
                     lower_all_targets();
@@ -447,9 +454,8 @@ void setup() {
     }
 
     display.setBrightness(0x09);
+    display.clear();
     randomSeed(analogRead(A5)); // Generates seed from noise
-
-    game_init();
 }
 
 void loop() {
@@ -464,5 +470,6 @@ void loop() {
         case PLAYING_STATE: playing_state(now); break;
         case WIN_STATE: win_state(now); break;
         case LOSS_STATE: loss_state(now); break;
+        default: break;
     }
 }
